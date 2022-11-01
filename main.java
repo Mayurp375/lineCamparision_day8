aa3c64be8f5bd5a30e0e797e0008186d4198f715
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-11-01 14:30:01 +0530

    main file

---
+++ main.java
@@ -0,0 +1,9 @@
+package lineComarision;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("by non static");
+        lengh lengh = new lengh();//object
+        lengh.lengthCalculater();
+    }
+}
